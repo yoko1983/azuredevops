7aebf93b1670609c31cf312a5dc6a4b632dc6353
yoko1983 <yoko1983@gmail.com>
2022-08-11 20:10:51 +0900

    微修正

--- func_mytest.py
+++ func_mytest.py
@@ -5,7 +5,7 @@ logger = getLogger(__name__)
 
 def print_pr_dict():
     try: 
-        pr_dict = func.get_pr_dict(3, 'master')
+        pr_dict = func.get_pr_dict(3, 'sitest')
         for pr_id in pr_dict.keys():
             logger.info(pr_dict[pr_id].pr_id)
     except Exception as e:
@@ -16,9 +16,9 @@ def print_pr_dict():
 logger.info('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
 print_pr_dict()
 logger.info('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-func.print_changed_filepath_dict_by_pr(3, 'master')
+func.print_changed_filepath_dict_by_pr(3, 'sitest')
 logger.info('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-func.print_changed_filepath_dict_by_repo(3, 'master')
+func.print_changed_filepath_dict_by_repo(3, 'ptctrt')
 logger.info('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-logger.info('CheckResult: ' + str(func.check_merged(3, 'master')))
+logger.info('CheckResult: ' + str(func.check_merged(3, 'sitest')))
 
